7baf0ca3d694319c27d4207898af5959534c0adb
Viet <stokyv@users.noreply.github.com>
2025-01-28 00:50:50 +0700

    re-install venv

--- drag_and_drop_zip.bat
+++ drag_and_drop_zip.bat
@@ -4,8 +4,12 @@ rem Check if a file was dropped
 if "%~1" == "" goto end
 
 call .venv\Scripts\activate.bat
+
+rem echo Checking Python path...
+rem where python
+
 rem Run the Python script
-py "script.py" "%~1"
+"E:\Github\resize-image-zipfile\.venv\Scripts\python.exe" "script.py" "%~1"
 
 
 rem Keep the command prompt window open until a key is pressed
